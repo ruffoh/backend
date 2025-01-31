575dd77b92bf9aaf19c8d30d9ddd7388458ba9a0
GiacomoRuffoni <giacomo.ruffoni@antonioscarpa.edu.it>
2025-01-31 16:31:50 +0100

    ok

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,12 +3,14 @@ import { LoginRequestDto, RegisterRequestDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
 import { isApplicationError } from '@utils/error/errors';
 import { mapProfileEntityToProfileDto } from '../profiles/profile.mapper';
+import { Public } from './auth.decorator';
 
 @Controller('/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/register')
+  @Public()
   async register(@Body() body: RegisterRequestDto) {
     const result = await this.authService.register(body);
 
@@ -22,6 +24,7 @@ export class AuthController {
 **/
   }
   @Post('/login')
+  @Public()
   async login(@Body() body: LoginRequestDto) {
     return this.authService.login(body);
   }
--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -6,19 +6,41 @@ import {
 } from '@nestjs/common';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
+import { IS_PUBLIC_KEY } from './auth.decorator';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly jwtService: JwtService,
+  ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    // se la rotta in qualche modo è pubblica non continuo a validare ma ritorno true
+
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest<Request>();
     if (!request.headers.authorization) {
       throw new UnauthorizedException();
       //  'il tuo header non contiene authorization sfigato non puoi',
     }
-
-    console.log(request.headers);
+    const token = request.headers.authorization.replace('Bearer ', '');
+    console.log(token);
+    try {
+      this.jwtService.verify(token);
+    } catch (error) {
+      throw new UnauthorizedException(error);
+    }
     return true;
   }
 }
